ad84cf5c7b444c1c0cc2e8e8562312c64ce4dea0
xiaohj <719887163@qq.com>
2022-03-20 01:30:26 +0800

    《2-4 Tab组件实现》，制作导航组件，未完成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <MyHeader/>
+  <NavigationBar/>
 </template>
 
 <script>
 import MyHeader from '@/components/MyHeader.vue'
+import NavigationBar from '@/components/NavigationBar.vue'
 export default {
   components: {
-    MyHeader
+    MyHeader,
+    NavigationBar
   }
 }
 </script>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tab">
+    <router-link
+      class="tab-item"
+      v-for="tab in tabs"
+      v-bind:key="tab.path"
+      v-bind:to="tab.path"
+    >
+      <span class="tab-link">{{tab.name}}</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavigationBar',
+  data: function () {
+    return {
+      tabs: [
+        { name: '推荐', path: '/recommend' },
+        { name: '歌手', path: '/singer' },
+        { name: '排行', path: '/top-list' },
+        { name: '搜索', path: '/search' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variable.scss";
+  .tab {
+    display: flex;
+    height: 44px;
+    line-height: 44px;
+    font-size: $font-size-medium;
+
+    .tab-item {
+      flex: 1; // 让弹性盒中的子元素有相同的长度
+    }
+  }
+</style>
