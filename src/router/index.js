b81fcb56e940274135f09ff48c3ca1b52c55880b
xiaohj <719887163@qq.com>
2022-06-04 14:49:17 +0800

    不使用路由异步加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const PageRecommend = () => import('@/views/PageRecommend.vue'/* webpackChunkName: 'PageRecommend' */)
-const PageSearch = () => import('@/views/PageSearch.vue'/* webpackChunkName: 'PageSearch' */)
-const PageSinger = () => import('@/views/PageSinger.vue'/* webpackChunkName: 'PageSinger' */)
-const PageTopList = () => import('@/views/PageTopList.vue'/* webpackChunkName: 'PageTopList' */)
-const PageSingerDetail = () => import('@/views/PageSingerDetail.vue'/* webpackChunkName: 'PageSingerDetail' */)
-const PageAlbum = () => import('@/views/PageAlbum.vue'/* webpackChunkName: 'PageAlbum' */)
-const PageTopDetail = () => import('@/views/PageTopDetail.vue'/* webpackChunkName: 'PageTopDetail' */)
+// const PageRecommend = () => import('@/views/PageRecommend.vue'/* webpackChunkName: 'PageRecommend' */)
+// const PageSearch = () => import('@/views/PageSearch.vue'/* webpackChunkName: 'PageSearch' */)
+// const PageSinger = () => import('@/views/PageSinger.vue'/* webpackChunkName: 'PageSinger' */)
+// const PageTopList = () => import('@/views/PageTopList.vue'/* webpackChunkName: 'PageTopList' */)
+// const PageSingerDetail = () => import('@/views/PageSingerDetail.vue'/* webpackChunkName: 'PageSingerDetail' */)
+// const PageAlbum = () => import('@/views/PageAlbum.vue'/* webpackChunkName: 'PageAlbum' */)
+// const PageTopDetail = () => import('@/views/PageTopDetail.vue'/* webpackChunkName: 'PageTopDetail' */)
+import PageRecommend from '@/views/PageRecommend.vue'
+import PageSearch from '@/views/PageSearch.vue'
+import PageSinger from '@/views/PageSinger.vue'
+import PageTopList from '@/views/PageTopList.vue'
+import PageSingerDetail from '@/views/PageSingerDetail.vue'
+import PageAlbum from '@/views/PageAlbum.vue'
+import PageTopDetail from '@/views/PageTopDetail.vue'
 
 const routes = [
   {
